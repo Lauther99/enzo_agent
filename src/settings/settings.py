027e58d0f1566e26cbcbdb3469922bf070c681bf
Lauther99 <lautherharold27@gmail.com>
2025-01-11 01:23:51 -0500

    created settings file, keep secure your secrets

---
+++ src/settings/settings.py
@@ -0,0 +1,15 @@
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+class Config:
+    GROQ_API_KEY = env("GROQ_API_KEY")
+    HF_TOKEN = env("HF_TOKEN")
+    SECRET_KEY = env("SECRET_KEY")
+    FIREBASE_CREDENTIALS_PATH = env("FIREBASE_CREDENTIALS_PATH")
+    CLIENT_SECRET_PATH = env("CLIENT_SECRET_PATH")
+    REDIRECT_URI = env("REDIRECT_URI")
+    WHATSAPP_URL = env("WHATSAPP_URL")
+    META_TOKEN = env("META_TOKEN")
+
