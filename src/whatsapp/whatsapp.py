1ee4654d188d09b21939ad2c872f9d4b19ef04b0
Lauther99 <lautherharold27@gmail.com>
2025-01-12 17:07:58 -0500

    implemented new changes from memory and user manager class

--- src/whatsapp/whatsapp.py
+++ src/whatsapp/whatsapp.py
@@ -7,6 +7,7 @@ from src.settings.settings import Config
 from src.components.memory import Memory
 from src.agent.agent_ import Agent, AgentExecutor
 from huggingface_hub import InferenceClient
+from src.firebase.users_manager import UserManager
 
 tools: dict[str, BaseTool] = {
     "email_scheduler": email_scheduler,
@@ -16,58 +17,32 @@ GROQ_API_KEY = Config.GROQ_API_KEY
 HF_API_KEY = Config.HF_TOKEN
 
 
-async def chat_manager(db: firestore.Client, msg_data: wsp_types.Props):
-    message = msg_data.messageInfo.content
-    message_type = msg_data.messageInfo.type
-    user_phone = msg_data.userPhoneNumber
-    waid = msg_data.id
+def chat_manager(db: firestore.Client, message_data: wsp_types.Props):
+    message = message_data.messageInfo.content
+    message_type = message_data.messageInfo.type
 
-    memory = Memory(
-        tools=tools,
-        db=db,
-        user_phone=user_phone,
-    )
+    user_phone = message_data.userPhoneNumber
+    bot_phone = message_data.botPhoneNumber
+    waid = message_data.id
 
+    user_manager = UserManager(db, user_phone, bot_phone, waid)
+    memory = Memory(tools=tools, user_manager=user_manager)
     collector = Collector()
 
+
     if message_type == "text":
-        memory.add_user_message(message, waid)
-        memory.save_memory()
         
-        agent = Agent.from_HF_llama3_3_70b_instruct(HF_API_KEY, memory, collector)
-        agent_executor = AgentExecutor(agent=agent, tools=tools)
+        if waid not in user_manager.user_document.current_waids:
+            memory.add_user_message(message, waid)
+            # memory.save_memory()
+            "hay que implementar el objeto para un mejor manejor de lo que se va guardando"
+            agent = Agent.from_HF_llama3_3_70b_instruct(HF_API_KEY, memory, collector)
+            agent_executor = AgentExecutor(agent=agent, tools=tools)
 
-        agent_executor.invoke()
+            agent_executor.invoke()
 
-        agent_executor.save_memory()
-        
+            user_manager.save_to_chat()
+        return
+
         # txt, messages_list, usage  = chat(llm=model_1, chat_messages=m_list, has_stream=True)
         # logging.info(txt)
-
-
-
-def chat_manager_2(db: firestore.Client, msg_data: wsp_types.Props, client: InferenceClient):
-    message = msg_data.messageInfo.content
-    message_type = msg_data.messageInfo.type
-    user_phone = msg_data.userPhoneNumber
-    waid = msg_data.id
-
-    memory = Memory(
-        tools=tools,
-        db=db,
-        user_phone=user_phone,
-    )
-
-    collector = Collector()
-
-    if message_type == "text":
-        # chat_template = memory.get_messages_chat_template()
-        memory.add_user_message(message, waid)
-        memory.save_memory()
-        
-        agent = Agent.from_HF_llama3_3_70b_instruct(HF_API_KEY, memory, collector)
-        agent_executor = AgentExecutor(agent=agent, tools=tools)
-
-        agent_executor.invoke()
-
-        agent_executor.save_memory()
