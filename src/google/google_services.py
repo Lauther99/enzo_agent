7abe42658318a8d02ea6d876fea769125661ea8f
Lauther99 <lautherharold27@gmail.com>
2025-01-13 01:31:56 -0500

    created caledndar services

--- src/google/google_services.py
+++ src/google/google_services.py
@@ -13,12 +13,17 @@ import base64
 import logging
 import random
 import string
+from src.utils.utils import convert_to_timezone, generate_random_string
+from src.google.types import CalendarEvent
+
 
 scopes = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/meetings.space.created",
+    "https://www.googleapis.com/auth/calendar"
 ]
 
 # Inicializa Firebase Admin
@@ -43,18 +48,9 @@ def google_login(user_manager: UserManager):
 
     short_id = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
 
-    # user_doc = user_ref.get().to_dict()
     new_map = {short_id: authorization_url}
     user_manager.user_document.url_map.update(new_map)
 
-    # current_url_maps[short_id] = authorization_url
-
-    # user_ref.update(
-    #         {
-    #             "url_map": current_url_maps,
-    #         }
-    #     )
-    
     short_url = f"{Config.REDIRECT_URI}/google-auth?user={user_manager.user_phone}&to={short_id}"
     return {"auth_url": short_url}
 
@@ -68,20 +64,20 @@ def refresh_access_token(credentials: Credentials) -> Credentials | None:
         current_time = datetime.now()
         # Comparar si el token ha expirado
         if expiry_time <= current_time:
-            print("El token ha expirado, renovando...")
+            logging.error("El token ha expirado, renovando...")
             # Intentar renovar el token usando el refresh_token
             if credentials.refresh_token:
                 credentials.refresh(GoogleRequest())
-                print("Token renovado exitosamente.")
+                logging.error("Token renovado exitosamente.")
                 return credentials
             else:
-                print("No se puede renovar el token, no hay refresh_token disponible.")
+                logging.error("No se puede renovar el token, no hay refresh_token disponible.")
                 return None
         else:
-            print("El token todavía es válido.")
+            logging.error("El token todavía es válido.")
             return credentials
     else:
-        print("La expiración del token no está disponible.")
+        logging.error("La expiración del token no está disponible.")
         return None
     
 def send_google_email(c: Credentials, to_emails: str, subject: str, body: str):
@@ -107,5 +103,196 @@ def send_google_email(c: Credentials, to_emails: str, subject: str, body: str):
             logging.info(f"Correo enviado a {to_email}")
         return {"success": True, "message": f"Correo enviado a: {', '.join(to_emails)}"}
     except Exception as e:
-        traceback.print_exc()
+        traceback.logging.error_exc()
         return {"success": False,"message": str(e)}
+
+def list_calendar_events(
+    creds: Credentials, calendar_id="primary", *, date_min: str, date_max: str
+):
+    """
+    Retrieves events from a user's Google Calendar.
+
+    Args:
+        calendar_id (str): The ID of the calendar to retrieve events from. Defaults to 'primary', which refers to the user's primary calendar.
+        date_after (str): The date after events occur in 'YYYY-MM-DD' format.
+        date_before (str): The date before events occur in 'YYYY-MM-DD' format. 
+    Returns:
+        list: A list of calendar events, where each event is represented as a dictionary containing event details.
+    """
+
+    try:
+        # Crear credenciales desde el diccionario
+        # Construir el servicio de la API de Google Calendar
+        service = build("calendar", "v3", credentials=creds)
+
+        # Llamar a la API para obtener eventos
+        events_result = (
+            service.events()
+            .list(
+                calendarId=calendar_id,
+                timeMin=convert_to_timezone(date_min),
+                timeMax=convert_to_timezone(date_max),
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        events_formated = [CalendarEvent.from_dict(ev) for ev in events]
+        
+        return events_formated
+
+    except Exception as e:
+        logging.error(f"Error al recuperar eventos: {e}")
+        traceback.logging.error_exc()
+        return []
+    
+def edit_calendar_event(
+    user_credentials: Credentials,
+    event_id: str,
+    summary: str = None,
+    description: str = None,
+    datetime_start: str = None,
+    datetime_end: str = None,
+    attendees_emails: list[str] = None,
+    has_conference: bool = True,
+    calendar_id="primary",
+):
+    updated_data = {}
+    if summary and summary.strip():
+        updated_data["summary"] = summary
+    if description and description.strip():
+        updated_data["description"] = description
+    if datetime_start and datetime_start.strip():
+        updated_data["start"] = {
+            "dateTime": convert_to_timezone(datetime_start),  # Nueva hora de inicio
+            "timeZone": Config.TIMEZONE,
+        }
+    if datetime_end and datetime_end.strip():
+        updated_data["end"] ={
+            "dateTime": convert_to_timezone(datetime_end),  # Nueva hora de fin
+            "timeZone": Config.TIMEZONE,
+        }
+    if attendees_emails:
+        updated_data["attendees"] = {
+        "attendees": [{"email": e for e in attendees_emails}],
+    }
+
+    if has_conference:
+        conference_data = {
+        "conferenceData": {
+            "createRequest": {
+                "requestId": generate_random_string(20),  # Debe ser único por solicitud
+                "conferenceSolutionKey": {"type": "hangoutsMeet"},
+            }
+        },
+    }
+        updated_data.update(conference_data)
+
+    try:
+        # Crear credenciales desde el diccionario
+
+        # Construir el servicio de la API de Google Calendar
+        service = build("calendar", "v3", credentials=user_credentials)
+
+        # Recuperar el evento existente
+        event = service.events().get(calendarId=calendar_id, eventId=event_id).execute()
+
+        # Actualizar los campos necesarios
+        for key, value in updated_data.items():
+            event[key] = value
+
+            if not has_conference and event.get("conferenceData"):
+                event["conferenceData"] = None
+
+        # Actualizar el evento en el calendario
+        updated_event = (
+            service.events()
+            .update(
+                calendarId=calendar_id,
+                eventId=event_id,
+                body=event,
+                sendUpdates="all",
+                conferenceDataVersion=1,
+            )
+            .execute()
+        )
+
+        return updated_event
+
+    except Exception as e:
+        logging.error(f"Error al actualizar el evento: {e}")
+        return None
+
+def create_calendar_event(
+    user_credentials: Credentials,
+    summary: str,
+    description: str,
+    datetime_start: str,
+    datetime_end: str,
+    attendees_emails: list[str] = [],
+    calendar_id="primary",
+):
+    event_data = {}
+    if summary and summary.strip():
+        event_data["summary"] = summary
+    if description and description.strip():
+        event_data["description"] = description
+    if datetime_start and datetime_start.strip():
+        event_data["start"] = {
+            "dateTime": convert_to_timezone(datetime_start),  # Nueva hora de inicio
+            "timeZone": Config.TIMEZONE,
+        }
+    if datetime_end and datetime_end.strip():
+        event_data["end"] ={
+            "dateTime": convert_to_timezone(datetime_end),  # Nueva hora de fin
+            "timeZone": Config.TIMEZONE,
+        }
+    if attendees_emails:
+        event_data["attendees"] = {
+        "attendees": [{"email": e for e in attendees_emails}],
+    }
+
+    try:
+        # Construir el servicio de la API de Google Calendar
+        service = build("calendar", "v3", credentials=user_credentials)
+
+        # Crear el evento en el calendario
+        created_event = (
+            service.events()
+            .insert(
+                calendarId=calendar_id,
+                body=event_data,
+                sendUpdates="all",
+                conferenceDataVersion=1,  # Habilita la integración con Google Meet
+            )
+            .execute()
+        )
+
+        created_event_formatted = CalendarEvent.from_dict(created_event)
+
+        hangout_added = edit_calendar_event(user_credentials, created_event_formatted.event_id)
+
+        return hangout_added
+
+    except Exception as e:
+        logging.error(f"Error al crear el evento: {e}")
+        return None
+    
+def list_calendars_types(user_credentials: Credentials):
+    service = build("calendar", "v3", credentials=user_credentials)
+
+    # Obtener la lista de calendarios
+    try:
+        calendars = service.calendarList().list().execute()
+
+        res = [
+            f"""Nombre: {calendar['summary']}, ID: {calendar['id']}, is_primary: {calendar.get("primary", False)}"""
+            for calendar in calendars["items"]
+        ]
+
+        return res
+    except Exception as e:
+        logging.error(f"Error al listar los calendarios: {e}")
+        return []
--- src/google/types.py
+++ src/google/types.py
@@ -1,7 +1,88 @@
-
-
+from datetime import datetime
 
 class DecodeJWTResponse:
     def __init__(self, is_valid, response):
         self.is_valid = is_valid
         self.response = response
+
+
+class CalendarEvent:
+    def __init__(
+        self,
+        event_id: str,
+        summary: str,
+        description: str,
+        start: str,
+        end: str,
+        attendees: list[str] = [],
+        hangout_link: str | None = None,
+    ):
+        self.event_id = event_id
+        self.summary = summary
+        self.description = description
+        self.start = start
+        self.end = end
+        self.attendees = attendees
+        self.hangout_link = hangout_link
+
+    def to_dict(self) -> dict:
+        """
+        Converts the CalendarEvent instance into a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the CalendarEvent instance.
+        """
+        return {
+            "event_id": self.event_id,
+            "summary": self.summary,
+            "description": self.description,
+            "start": self._format_date(self.start, prefix="From"),
+            "end": self._format_date(self.end, prefix="To"),
+            "attendees": self.attendees,
+            "hangout_link": self.hangout_link,
+        }
+    
+    @classmethod
+    def from_dict(cls, data: dict):
+        """
+        Creates a CalendarEvent instance from a dictionary.
+
+        Args:
+            data (dict): A dictionary containing the event data.
+
+        Returns:
+            CalendarEvent: An instance of the CalendarEvent class.
+        """
+        s=data.get("start", {}).get("dateTime", "")
+        e=data.get("end", {}).get("dateTime", "")
+
+        s = cls._format_date(s, prefix="From")
+        e = cls._format_date(e, prefix="To")
+
+        return cls(
+            event_id=data.get("id", ""),
+            summary=data.get("summary", ""),
+            description=data.get("description", ""),
+            start=s,
+            end=e,
+            attendees=data.get("attendees", []),
+            hangout_link=data.get("hangoutLink", None),
+        )
+    
+    @staticmethod
+    def _format_date(date_str: str, prefix: str = "From") -> str:
+        """
+        Formats an ISO 8601 date string into a human-readable format in Spanish.
+        
+        Args:
+            date_str (str): The date string in ISO 8601 format.
+            prefix (str): A prefix like "Desde" or "Hasta".
+        
+        Returns:
+            str: The formatted date string.
+        """
+        try:
+            dt = datetime.fromisoformat(date_str)
+            return f"{prefix} {dt.day}/{dt.strftime('%B')}/{dt.year} at {dt.strftime('%H:%M')}."
+        except ValueError:
+            return date_str
