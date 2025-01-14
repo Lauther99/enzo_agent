96d071cd3514d58329235834dd49de2a5c60f89e
Lauther99 <lautherharold27@gmail.com>
2025-01-14 10:47:47 -0500

    improved function to set action: - Now throw errors when a description of arguments is missing. - Improved way to retrieve descriptions for arguments and for the tool

--- src/components/tool.py
+++ src/components/tool.py
@@ -20,31 +20,31 @@ authorized_types = {
     "None": "null",  # Para funciones que retornan `None`
 }
 
+
 class BaseTool:
     def __init__(self, func: Callable[..., Any]):
-        self.description = func.__doc__  # Documentación de la función
         self.name = func.__name__  # Nombre de la función
+
+        self.description = self._get_description(func)  # Documentación de la función
         self.inputs = self._extract_inputs(func)  # Tipos y descripciones de entrada
-        return_type = func.__annotations__.get('return', type(None))  # Tipo de retorno o `NoneType`
-        self.output_type: str = authorized_types.get(return_type, "unknown")  # Mapeo o "unknown" si no está definido
+
+        return_type = func.__annotations__.get(
+            "return", type(None)
+        )  # Tipo de retorno o `NoneType`
+        self.output_type: str = authorized_types.get(
+            return_type
+        )  # Mapeo o "unknown" si no está definido
         self._func = func  # Referencia a la función decorada
 
-    # def _extract_inputs(self, func: Callable[..., Any]) -> Dict[str, Dict[str, str]]:
-    #     """Extrae tipos y descripciones de los argumentos de la función."""
-    #     inputs = {}
-    #     # Parsear el docstring para obtener descripciones de los argumentos
-    #     docstring = func.__doc__ or ""
-    #     arg_descriptions = self._parse_arg_descriptions(docstring)
+    def _get_description(self, func: Callable[..., Any]):
+        description = func.__doc__.split("Args")[0].strip() if func.__doc__ else ""
+        if description:
+            return description
+        else:
+            raise ValueError(
+                "The provided function does not have a valid description in its docstring."
+            )
 
-    #     for arg, arg_type in func.__annotations__.items():
-    #         if arg == "return":
-    #             continue
-    #         # Construir la entrada enriquecida
-    #         inputs[arg] = {
-    #             "type": authorized_types.get(arg_type, "unknown"),
-    #             "description": arg_descriptions.get(arg, "No description available.")
-    #         }
-    #     return inputs
     def _extract_inputs(self, func: Callable[..., Any]) -> Dict[str, Dict[str, str]]:
         """Extrae tipos y descripciones de los argumentos de la función."""
         inputs = {}
@@ -54,29 +54,39 @@ class BaseTool:
 
         # Obtener los parámetros de la función
         signature = inspect.signature(func)
+
         for param_name, param in signature.parameters.items():
-            if param_name == "self" or param_name == "cls":
+            if param_name == "self" or param_name == "cls" or param_name == "kwargs":
                 continue  # Ignorar 'self' y 'cls' para métodos de clase
 
             # Obtener el tipo del argumento
-            param_type = param.annotation if param.annotation is not param.empty else "unknown"
-            param_type_str = authorized_types.get(str(param_type), "unknown")
+            param_type = (
+                param.annotation if param.annotation is not param.empty else None
+            )
+            param_type_str = authorized_types.get(str(param_type), None)
+
+            if not param_type or not param_type_str:
+                raise ValueError(
+                    f"Not supported '{param_type_str}' type for '{param_name}'. Only supported: {", ".join([item for _, item in authorized_types.items()])}."
+                )
 
             # Obtener la descripción del argumento, si está en el docstring
-            description = arg_descriptions.get(param_name, "No description available.")
-            
-            # Agregar al diccionario de inputs
-            inputs[param_name] = {
-                "type": param_type_str,
-                "description": description
-            }
-        return inputs
+            description = arg_descriptions.get(param_name, None)
+            if not description:
+                raise ValueError(f"Not valid description for '{param_name}'.")
 
+            # Agregar al diccionario de inputs
+            if param_name != "kwargs":
+                inputs[param_name] = {
+                    "type": param_type_str,
+                    "description": description,
+                }
+        return inputs
 
     @staticmethod
     def _parse_arg_descriptions(docstring: str) -> Dict[str, str]:
         """Extrae descripciones de argumentos desde el docstring."""
-        descriptions ={}
+        descriptions = {}
         args_section = re.search(r"Args:\s*(.*)", docstring, re.DOTALL)
         pattern = re.compile(r"^\s*(\w+):\s*(.+)$", re.MULTILINE)
 
@@ -84,15 +94,20 @@ class BaseTool:
             args_text = args_section.group(1)
             matches = pattern.findall(args_text)
             for arg_name, description in matches:
-                descriptions[arg_name] = description.strip()
-        
+                if description.strip():
+                    descriptions[arg_name] = description.strip()
+                else:
+                    raise ValueError(f"Not valid description for '{arg_name}'.")
+
         return descriptions
-    
+
     def __call__(self, *args, **kwargs):
         return self._func(*args, **kwargs)  # Llama a la función decorada
 
+
 def set_action(func: Callable[..., Any]) -> BaseTool:
-    return BaseTool(func)  
+    return BaseTool(func)
+
 
 class BaseToolResponse:
     def __init__(
@@ -144,20 +159,31 @@ class BaseToolResponse:
             f"usage={self.usage}, "
             f"metadata={self.metadata})"
         )
-    
+
 
 class BaseToolCollector(ABC):
     def __init__(self):
         self.ToolResponse: BaseToolResponse = None
         self.llm_responses: list[str] = []
 
-    @abstractmethod
     def add_tool_response(
         self,
         call_id: uuid.UUID | str,
         tool_name: str,
-        response: any,
+        tool_response: list[dict],
+        tool_friendly_response: list[dict],
         response_type: type,
-        llm_responses: list[str] = [],
+        usage={},
+        metadata={},
     ):
-        pass
+        self.ToolResponse = BaseToolResponse(
+            call_id=call_id,
+            tool_name=tool_name,
+            tool_response=tool_response,
+            tool_friendly_response=tool_friendly_response,
+            response_type=response_type,
+            llm_responses=self.llm_responses,
+            usage=usage,
+            metadata=metadata,
+        )
+        return self.ToolResponse
