2bd474bd41029a8b2020bc4c41d9d67e5e901b2c
Lauther99 <lautherharold27@gmail.com>
2025-01-14 10:40:56 -0500

    Changes in agent and whatsapp files - In general, moved whatsapp send message function from agent file to whatsapp file, for a better comprenhension - Now agent returns a response, that is managed in whatsapp file and then update the memory - Created a variable outside the agent object, calling a default agent to be used in all app, now AgentExecutor accepts memory and collectors as arguments

--- src/agent/agent_.py
+++ src/agent/agent_.py
@@ -1,6 +1,6 @@
 from src.agent.tools.collectors import Collector
 from src.components.memory import Memory
-from src.components.llms import BaseGenericLLM, GenericLLM
+from src.components.llms import BaseGenericLLM, GenericLLM, default_llms
 from src.components.prompt import ChatTemplate
 from src.components.tool import BaseTool
 import uuid
@@ -14,39 +14,6 @@ from src.settings.settings import Config
 
 WHATSAPP_URL = Config.WHATSAPP_URL
 
-def send_whatsapp_message(
-    *,
-    to_phone: str,
-    message: str,
-) -> str:
-    try:
-        data = {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": to_phone,
-            "type": "text",
-            "text": {
-                "preview_url": True,
-                "body": message[:4094],  # Limitamos a 4094 caracteres
-            },
-        }
-        response = WhatsAppSession.post(f"{WHATSAPP_URL}/messages", json=data)
-
-        if response.status_code == 200:
-            r = response.json()
-            waid = r["messages"][0]["id"]
-
-            print("Mensaje enviado:", response.json())
-            return waid
-        else:
-            print(f"Error {response.status_code}: {response.text}")
-
-            return ""
-
-    except Exception as e:
-        logging.error(e)
-        return ""
-    
 def chat(
     *,
     llm: BaseGenericLLM,
@@ -67,7 +34,6 @@ def chat(
             has_stream=has_stream,
         ):
             if c:
-                # logging.info(f"{c}")
                 txt += c
             if u:
                 usage.update(u)
@@ -110,19 +76,19 @@ def base_agent_chat_generation_2(
     final_answer = action_json["action_input"]["answer"] if action == "final_answer" else ""
     markdown_info = ""
 
-    current_scratchpad = """"""
-    if thought.strip():
-        current_scratchpad += f"Thought: {thought}\n"
-    if action.strip():
-        current_scratchpad += f"Action: {action}\n"
-    if parameters:
-        current_scratchpad += f"Parameters: {parameters}\n"
-    if final_answer.strip():
-        current_scratchpad += f"Answer: {final_answer}\n"
-    if markdown_info.strip():
-        current_scratchpad += f"Markdown info: {markdown_info}\n"
+    # current_scratchpad = """"""
+    # if thought.strip():
+    #     current_scratchpad += f"Thought: {thought}\n"
+    # if action.strip():
+    #     current_scratchpad += f"Action: {action}\n"
+    # if parameters:
+    #     current_scratchpad += f"Parameters: {parameters}\n"
+    # if final_answer.strip():
+    #     current_scratchpad += f"Answer: {final_answer}\n"
+    # if markdown_info.strip():
+    #     current_scratchpad += f"Markdown info: {markdown_info}\n"
 
-    print(current_scratchpad, "\n\n")
+    # print(current_scratchpad, "\n\n")
 
     response_dict = Base_Agent_Response(
         thought=thought,
@@ -174,15 +140,12 @@ class Agent:
     @classmethod
     def from_HF_llama3_3_70b_instruct(
         cls,
-        api_key: str,
-        memory: Memory,
-        collector: Collector,
     ):
         agent_instance = cls()
-        agent_instance.memory = memory
-        agent_instance.collector = collector
+        # agent_instance.memory = memory
+        # agent_instance.collector = collector
 
-        agent_instance.llm_model = GenericLLM.from_HF_llama3_3_70b_instruct(api_key=api_key)
+        agent_instance.llm_model = default_llms["from_HF_llama3_3_70b_instruct"]
         return agent_instance
     
 
@@ -191,7 +154,7 @@ class Agent:
         tools: dict[str, BaseTool],
         max_iterarions=5,
         current_iteration=1,
-    ):
+    ) -> Base_Agent_Response:
         response_dict: Base_Agent_Response = base_agent_chat_generation_2(
             llm=self.llm_model,
             chat_template=self.memory.get_messages_chat_template(),
@@ -204,13 +167,13 @@ class Agent:
         logging.info("\n\n\n")
 
         if response_dict.action == "final_answer":
-            waid = send_whatsapp_message(
-                to_phone=self.memory.user_manager.user_phone,
-                message=response_dict.final_answer,
-            )
+            # waid = send_whatsapp_message(
+            #     to_phone=self.memory.user_manager.user_phone,
+            #     message=response_dict.final_answer,
+            # )
 
-            self.memory.add_assistant_message(response_dict.content, waid)
-            return  # Aqui termina el loop
+            # self.memory.add_assistant_message(response_dict.content, waid)
+            return  response_dict
 
         else:
             action_name = response_dict.action
@@ -228,8 +191,6 @@ class Agent:
                 try:
                     parameters["collector"] = self.collector
                     parameters["memory"] = self.memory
-                    # parameters["user_phone"] = self.memory.user_manager.user_phone
-                    # parameters["db"] = self.memory.user_manager.db
 
 
                 except Exception as e:
@@ -250,12 +211,8 @@ class Agent:
                     logging.info(action_response_message)
 
                     self.memory.add_tool_message(action_response_message, action_name)
-                    # tool_message = self.memory.add_tool_message(action_response, tool_name=str(action_name), replied_message_id=assistant_message_id)
-                    # tool_message_id = tool_message.metadata["message_id"]
 
                 except Exception as e:
-                    # tool_message = self.memory.add_tool_message(e, tool_name=str(action), replied_message_id=assistant_message_id)
-                    # tool_message_id = tool_message.metadata["message_id"]
                     message = f"Action Response: {e}\n"
                     logging.debug(message)
                     self.memory.add_tool_message(message, action_name)
@@ -273,28 +230,36 @@ class Agent:
                         current_iteration + 1,
                     )
                 else:
-                    waid = send_whatsapp_message(
-                        to_phone=self.memory.user_manager.user_phone,
-                        message="Exceed number of iterations, try again later.",
-                    )
+                    # waid = send_whatsapp_message(
+                    #     to_phone=self.memory.user_manager.user_phone,
+                    #     message="Exceed number of iterations, try again later.",
+                    # )
                     
-                    self.memory.add_assistant_message(
-                        message="Exceed number of iterations, try again later.",
-                        waid=waid
+                    # self.memory.add_assistant_message(
+                    #     message="Exceed number of iterations, try again later.",
+                    #     waid=waid
+                    # )
+                    return Base_Agent_Response(
+                        final_answer=message
                     )
-
-                    return
-
 
 class AgentExecutor:
-    def __init__(self, agent: Agent, tools: Dict[str, BaseTool]):
+    def __init__(self, agent: Agent, tools: Dict[str, BaseTool], memory: Memory, collector: Collector):
         self.agent = agent
         self.tools = tools
+
+        self.agent.memory = memory
+        self.agent.collector = collector
 
     def invoke(self, *, max_iterations=10):
         max_iterations = max_iterations or 10
 
-        self.agent.agent_loop_2(
+        response = self.agent.agent_loop_2(
             tools=self.tools,
             max_iterarions=max_iterations,
         )
+
+        return response
+
+
+default_agent = Agent.from_HF_llama3_3_70b_instruct()
--- src/whatsapp/whatsapp.py
+++ src/whatsapp/whatsapp.py
@@ -4,18 +4,56 @@ import src.whatsapp.types as wsp_types
 from google.cloud import firestore
 from src.agent.tools.email_scheduler import email_scheduler
 from src.agent.tools.google_auth import google_auth_url
+from src.agent.tools.calendar_events import list_events, manage_events
 from src.settings.settings import Config
 from src.components.memory import Memory
-from src.agent.agent_ import Agent, AgentExecutor
+from src.agent.agent_ import AgentExecutor, default_agent
+from src.whatsapp.requests.requests_ import WhatsAppSession
 from src.firebase.users_manager import UserManager
+import logging
 
 tools: dict[str, BaseTool] = {
     "email_scheduler": email_scheduler,
     "google_auth_url": google_auth_url,
+    "list_events": list_events,
+    "manage_events": manage_events,
 }
 
-GROQ_API_KEY = Config.GROQ_API_KEY
-HF_API_KEY = Config.HF_TOKEN
+WHATSAPP_URL = Config.WHATSAPP_URL
+
+
+def send_whatsapp_message(
+    *,
+    to_phone: str,
+    message: str,
+) -> str:
+    try:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": to_phone,
+            "type": "text",
+            "text": {
+                "preview_url": True,
+                "body": message[:4094],
+            },
+        }
+        response = WhatsAppSession.post(f"{WHATSAPP_URL}/messages", json=data)
+
+        if response.status_code == 200:
+            r = response.json()
+            waid = r["messages"][0]["id"]
+
+            print("Mensaje enviado:", response.json())
+            return waid
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+
+            return ""
+
+    except Exception as e:
+        logging.error(e)
+        return ""
 
 
 def chat_manager(db: firestore.Client, message_data: wsp_types.Props):
@@ -30,19 +68,28 @@ def chat_manager(db: firestore.Client, message_data: wsp_types.Props):
     memory = Memory(tools=tools, user_manager=user_manager)
     collector = Collector()
 
-
     if message_type == "text":
-        
+
         if waid not in user_manager.user_document.current_waids:
             memory.add_user_message(message, waid)
-            # memory.save_memory()
-            "hay que implementar el objeto para un mejor manejor de lo que se va guardando"
-            agent = Agent.from_HF_llama3_3_70b_instruct(HF_API_KEY, memory, collector)
-            agent_executor = AgentExecutor(agent=agent, tools=tools)
+            
+            agent_executor = AgentExecutor(
+                agent=default_agent, tools=tools, memory=memory, collector=collector
+            )
 
-            agent_executor.invoke()
+            response = agent_executor.invoke()
 
+            waid = send_whatsapp_message(
+                to_phone=memory.user_manager.user_phone,
+                message=response.final_answer,
+            )
+            memory.add_assistant_message(response.content, waid)
             user_manager.save_to_chat()
+        return
+
+    elif message_type == "audio":
+        logging.info(message_data.to_json())
+
         return
 
         # txt, messages_list, usage  = chat(llm=model_1, chat_messages=m_list, has_stream=True)
